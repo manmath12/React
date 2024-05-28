0cb392d40f9b9d9139df4469591366d9e979686f
manmath12 <121674508+manmath12@users.noreply.github.com>
2024-05-28 11:05:47 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,7 @@
+
+const heading = React.createElement("h1",{id:"heading"},"hello wprld from insight");
+        
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+        
+root.render(heading)
